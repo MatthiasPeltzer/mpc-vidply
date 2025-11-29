8ff38e152f8a89d08bdd3ecb2c5cdc017bb4b2f0
Matthias <mail@mpeltzer.de>
2025-11-29 17:58:40 +0100

    feat: initial commit

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,24 +2,34 @@
 
 defined('TYPO3') or die();
 
-$_EXTKEY = 'mpc_vidply';
+/**
+ * This extension requires Composer and cannot be installed via TER/Extension Manager.
+ *
+ * Installation:
+ * composer require mpc/mpc-vidply
+ *
+ */
 
-$EM_CONF['mpc_vidply'] = array(
+$_EXTKEY = "mpc_vidply";
+
+$EM_CONF[$_EXTKEY] = [
     'title' => 'VidPly Player',
     'description' => 'Universal, Accessible Video & Audio Player for TYPO3. Includes support for HTML5 video/audio, YouTube, Vimeo, SoundCloud, HLS streaming, playlists, captions, transcripts, sign language, and full WCAG 2.1 AA accessibility compliance.',
     'category' => 'plugin',
-    'state' => 'stable',
-    'author' => 'Matthias Peltzer',
-    'author_email' => 'mail@mpeltzer.de',
-    'version' => '1.0.0',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '13.4.0-14.99.99',
             'fluid' => '13.4.0-14.99.99',
             'extbase' => '13.4.0-14.99.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-);
-
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Matthias Peltzer',
+    'author_email' => 'mail@mpeltzer.de',
+    'version' => '1.0.0',
+];
