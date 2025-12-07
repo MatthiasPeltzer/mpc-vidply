570969fa17ea066664319e131e0e6242a36cd408
Matthias <mail@mpeltzer.de>
2025-12-07 13:30:57 +0100

    fix: apply TCA migrations for TYPO3 13+ and make mpc-vidply independent from mp-core

---
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') || die();
+
+(static function (): void {
+    $newColumns = [
+        'tx_lang_code' => [
+            'label' => 'LLL:EXT:mpc_vidply/Resources/Private/Language/locallang_db.xlf:sys_file_reference.tx_lang_code',
+            'config' => [
+                'type' => 'input',
+                'size' => 10,
+                'max' => 10,
+                'eval' => 'trim',
+                'placeholder' => 'de',
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+            ],
+        ],
+        'tx_track_kind' => [
+            'label' => 'LLL:EXT:mpc_vidply/Resources/Private/Language/locallang_db.xlf:sys_file_reference.tx_track_kind',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    ['label' => 'LLL:EXT:mpc_vidply/Resources/Private/Language/locallang_db.xlf:sys_file_reference.tx_track_kind.captions', 'value' => 'captions'],
+                    ['label' => 'LLL:EXT:mpc_vidply/Resources/Private/Language/locallang_db.xlf:sys_file_reference.tx_track_kind.subtitles', 'value' => 'subtitles'],
+                    ['label' => 'LLL:EXT:mpc_vidply/Resources/Private/Language/locallang_db.xlf:sys_file_reference.tx_track_kind.descriptions', 'value' => 'descriptions'],
+                    ['label' => 'LLL:EXT:mpc_vidply/Resources/Private/Language/locallang_db.xlf:sys_file_reference.tx_track_kind.chapters', 'value' => 'chapters'],
+                    ['label' => 'LLL:EXT:mpc_vidply/Resources/Private/Language/locallang_db.xlf:sys_file_reference.tx_track_kind.metadata', 'value' => 'metadata'],
+                ],
+                'default' => 'captions',
+                'behaviour' => [
+                    'allowLanguageSynchronization' => true,
+                ],
+            ],
+        ],
+    ];
+
+    ExtensionManagementUtility::addTCAcolumns('sys_file_reference', $newColumns);
+})();
--- ext_tables.sql
+++ ext_tables.sql
@@ -1,4 +1,14 @@
 #
+# Table structure for table 'sys_file_reference'
+# Additional fields for VTT track files
+#
+CREATE TABLE sys_file_reference
+(
+	tx_lang_code  varchar(10) DEFAULT ''         NOT NULL,
+	tx_track_kind varchar(50) DEFAULT 'captions' NOT NULL
+);
+
+#
 # Table structure for table 'tx_mpcvidply_media'
 # Standalone media records that can be reused across the site
 #
